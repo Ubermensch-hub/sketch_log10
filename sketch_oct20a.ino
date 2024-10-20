1732793fbf26e0279f6ffe6e88ceba5f4ebc8824
Uber-hub <60351073+Ubermensch-hub@users.noreply.github.com>
2024-10-20 17:31:29 +0600

    Initial commit

---
+++ sketch_oct20a.ino
@@ -0,0 +1,33 @@
+#include <GyverOLED.h>
+#include <math.h>
+#include <stdio.h>
+GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
+void setup() {
+  oled.init();        // инициализация
+  oled.clear();       // очистка
+  oled.setScale(3);   // масштаб текста (1..4)
+  oled.home();        // курсор в 0,0
+}
+void loop() 
+{
+  
+    for (int i=0; i!=10; ++i)
+    {
+      oled.print("lg = ");
+      oled.setCursor(0, 4);
+      oled.print(log10(i));
+      delay(1500);
+      oled.clear();
+      oled.home();
+    }
+    for (int i=10; i != 0; --i)
+    {
+      oled.print("lg = ");
+      oled.setCursor(0, 4);
+      oled.print(log10(i));
+      delay(1500);
+      oled.clear();
+      oled.home();
+    }
+  
+}
